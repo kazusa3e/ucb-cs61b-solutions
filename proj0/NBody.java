def9c42f97c14d02252eb2ecdd51dd61bd6a0135
kazusa <i@kazusa.me>
2024-03-14 12:18:14 +0800

    creating an animation & printing the universe

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -48,5 +48,36 @@ public class NBody {
             planets[ix].draw();
         }
 
+        // creating an animation
+        StdDraw.enableDoubleBuffering();
+        for (double t = 0; t <= T; t += dt) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int ix = 0; ix != planets.length; ++ix) {
+                xForces[ix] = planets[ix].calcNetForceExertedByX(planets);
+                yForces[ix] = planets[ix].calcNetForceExertedByY(planets);
+            }
+            for (int ix = 0; ix != planets.length; ++ix) {
+                planets[ix].update(t, xForces[ix], yForces[ix]);
+            }
+            StdDraw.picture(0, 0, backgroundImage);
+            for (int ix = 0; ix != planets.length; ++ix) {
+                planets[ix].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+        }
+
+        // printing the universe
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        }
+
+        System.exit(0);
+
     }
 }
