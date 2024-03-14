fbfec5c9a1c8a8c49d9497e059e4fc1b012663c9
kazusa <i@kazusa.me>
2024-03-14 11:01:41 +0800

    readPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,23 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Planet[] readPlanets(String filePath) {
+        In in = new In(filePath);
+        int numPlanets = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planets = new Planet[numPlanets];
+        for (int ix = 0; ix != numPlanets; ++ix) {
+            Planet p = new Planet(
+                in.readDouble(),
+                in.readDouble(),
+                in.readDouble(),
+                in.readDouble(),
+                in.readDouble(),
+                in.readString()
+            );
+            planets[ix] = p;
+        }
+        return planets;
+    }
 }
