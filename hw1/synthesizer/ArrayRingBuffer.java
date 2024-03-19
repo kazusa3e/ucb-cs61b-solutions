bcab2c136fd606809f364d6c4ace5546c6d642b4
kazusa <i@kazusa.me>
2024-03-19 17:07:24 +0800

    (hw01) feat: ArrayRingBuffer

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,82 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+import javax.management.RuntimeErrorException;
+
+// Make sure to make this class and all of its methods public
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+
+    /* Index for the next dequeue or peek. */
+    private int first; // index for the next dequeue or peek
+
+    /* Index for the next enqueue. */
+    private int last;
+
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    private int idx(int index) {
+        return (index + this.capacity) % this.capacity;
+    }
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // Create new array with capacity elements.
+        // first, last, and fillCount should all be set to 0.
+        // this.capacity should be set appropriately. Note that the local variable
+        // here shadows the field we inherit from AbstractBoundedQueue, so
+        // you'll need to use this.capacity to set the capacity.
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        // Enqueue the item. Don't forget to increase fillCount and update last.
+        if (this.isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        this.rb[this.last] = x;
+        this.last = idx(this.last + 1);
+        this.fillCount += 1;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        // Dequeue the first item. Don't forget to decrease fillCount and update
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T ret = this.rb[this.first];
+        this.first = idx(this.first + 1);
+        this.fillCount -= 1;
+        return ret;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        // Return the first item. None of your instance variables should change.
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer is empty");
+        }
+        return this.rb[this.first];
+    }
+
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,19 +1,35 @@
 package synthesizer;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+/**
+ * Tests the ArrayRingBuffer class.
+ * 
+ * @author Josh Hug
  */
 
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(4);
+        assertTrue(arb.isEmpty());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        assertTrue(arb.isFull());
+        assertEquals((Integer) 1, arb.peek());
+        assertEquals((Integer) 1, arb.dequeue());
+        assertEquals((Integer) 2, arb.dequeue());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        assertEquals((Integer) 3, arb.peek());
+        assertTrue(arb.isFull());
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
-} 
+}
