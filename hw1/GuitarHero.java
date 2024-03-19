21f211404786a373d42352fc00f86ebd48ed6c39
kazusa <i@kazusa.me>
2024-03-19 17:51:23 +0800

    (hw01) feat: GuitarHero

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,44 @@
+import synthesizer.GuitarString;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar
+ * string sound
+ */
+public class GuitarHero {
+
+    private static final double CONCERT_A = 440.0;
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        GuitarString[] guitarStrings = new GuitarString[keyboard.length()];
+        for (int ix = 0; ix != keyboard.length(); ++ix) {
+            guitarStrings[ix] = new GuitarString(
+                    CONCERT_A * Math.pow(2, (ix - 24) / 12));
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                if (keyboard.indexOf(key) != -1)  {
+                    guitarStrings[keyboard.indexOf(key)].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0.0;
+            for (int ix = 0; ix != guitarStrings.length; ++ix) {
+                sample += guitarStrings[ix].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int ix = 0; ix != guitarStrings.length; ++ix) {
+                guitarStrings[ix].tic();
+            }
+        }
+    }
+}
