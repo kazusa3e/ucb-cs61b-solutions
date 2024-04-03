5e6514803dd0af91e7c75f1a5527621303996211
kazusa <i@kazusa.me>
2024-04-03 19:11:45 +0800

    (lab09) feat: BSTMap

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -2,6 +2,9 @@ package lab9;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
+
+import edu.princeton.cs.algs4.Queue;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
@@ -25,7 +28,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    private Node root;  /* Root node of the tree. */
+    private Node root; /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
 
     /* Creates an empty BSTMap. */
@@ -40,40 +43,73 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (key.compareTo(p.key) < 0 && p.left != null) {
+            return getHelper(key, p.left);
+        }
+        if (key.compareTo(p.key) > 0 && p.right != null) {
+            return getHelper(key, p.right);
+        }
+        if (key.compareTo(p.key) == 0) {
+            return p.value;
+        }
+        return null;
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (this.root == null) {
+            return null;
+        }
+        return this.getHelper(key, this.root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            this.size += 1;
+            Node node = new Node(key, value);
+            return node;
+        }
+        if (key.compareTo(p.key) == 0) {
+            p.value = value;
+            return p;
+        }
+        if (key.compareTo(p.key) < 0) {
+            p.left = putHelper(key, value, p.left);
+            return p;
+        }
+        if (key.compareTo(p.key) > 0) {
+            p.right = putHelper(key, value, p.right);
+            return p;
+        }
+        return null;
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        this.root = putHelper(key, value, this.root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,29 +117,118 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> ret = new TreeSet<>();
+        if (this.root == null) {
+            return ret;
+        }
+        Queue<Node> queue = new Queue<>();
+        queue.enqueue(this.root);
+        while (!queue.isEmpty()) {
+            Node n = queue.dequeue();
+            if (n.left != null) {
+                queue.enqueue(n.left);
+            }
+            if (n.right != null) {
+                queue.enqueue(n.right);
+            }
+            ret.add(n.key);
+        }
+        return ret;
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    private V removeHelper(K key, Node p) {
+        if (p == null) {
+            return null;
+        }
+        if (key.compareTo(p.key) < 0 && p.left != null) {
+            return removeHelper(key, p.left);
+        }
+        if (key.compareTo(p.key) > 0 && p.right != null) {
+            return removeHelper(key, p.right);
+        }
+        if (key.compareTo(p.key) == 0) {
+            V ret = p.value;
+            p = null;
+            this.size -= 1;
+            return ret;
+        }
+        return null;
+    }
+
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        // if (this.root == null) {
+        // return null;
+        // }
+        return removeHelper(key, this.root);
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    private V removeHelper(K key, V val, Node p) {
+        if (p == null) {
+            return null;
+        }
+        if (key.compareTo(p.key) < 0 && p.left != null) {
+            return removeHelper(key, val, p.left);
+        }
+        if (key.compareTo(p.key) < 0 && p.right != null) {
+            return removeHelper(key, val, p.right);
+        }
+        if (key.compareTo(p.key) == 0) {
+            V ret = p.value;
+            p = null;
+            this.size -= 1;
+            return ret;
+        }
+        return null;
+    }
+
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value. Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        return removeHelper(key, value, this.root);
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+
+        private Queue<Node> queue;
+
+        public BSTMapIterator() {
+            this.queue = new Queue<>();
+            if (BSTMap.this.root != null) {
+                this.queue.enqueue(BSTMap.this.root);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !this.queue.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            Node n = this.queue.dequeue();
+            if (n.left != null) {
+                this.queue.enqueue(n.left);
+            }
+            if (n.right != null) {
+                this.queue.enqueue(n.right);
+            }
+            return n.key;
+        }
+
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new BSTMapIterator();
     }
 }
